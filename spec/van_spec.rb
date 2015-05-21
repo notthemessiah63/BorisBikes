0f153d71c2d4198779bade2f77f6a0edcfbfdc54
Brian Newton <notthemessiah63@gmail.com>
2015-05-21 21:12:04 +0100

    added van spec and class

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../lib/docking_station'
 require_relative '../lib/van'
 describe Van do
-  station.dock(bike)
-  expect(station.bike_count).to eq 1
+  it 'should accept a bike' do
+    station.dock(bike)
+    expect(station.bike_count).to eq 1
+  end
 end
